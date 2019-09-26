6474dfee5123a649e1c8273958b664e28bc4429c
Shapkofil <ldshapkofil@gmail.com>
2019-09-26 23:17:04 +0300

    Face_calculator.py sorting now

--- face_calculator.py
+++ face_calculator.py
@@ -10,11 +10,12 @@ def compute(x,y):
 
 cube = compute([0,1,2,3],[4,5,6,7])
 tessaract = cube [:]
+tessaract.extend(compute([8,9,10,11],[12,13,14,15]))
 for face in cube:
 	for newface in compute(face,[x+8 for x in face]):
 		if not newface in tessaract:
 			tessaract.append(newface)
 
-print(tessaract)
+print(sorted(tessaract,key = lambda x: (x[0],x[1],x[2])))
 
 print(len(tessaract))
