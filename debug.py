76f50f1b40f7fc5fb221811d30c5ff1e6c7f4bd2
Shapkofil <ldshapkofil@gmail.com>
2019-09-26 21:29:31 +0300

    Implementation of 3d projections

--- debug.py
+++ debug.py
@@ -2,6 +2,6 @@ from tessaract import Tessaract
 import math
 
 tessaract = Tessaract([[-1,-1,-1,-1],[1,1,1,1]])
-print(tessaract.vertex)
+print(tessaract.vertcies)
 tessaract.rotate(math.pi*2)
-print(tessaract.vertex)
+print(tessaract.vertcies)
--- tessaract.py
+++ tessaract.py
@@ -1,8 +1,7 @@
 import numpy as np
+import math
 
-class Tessaract(object):
-	vertex = []
-
+class Tessaract:
 	def __init__(self,dimentions = [[-1,-1,-1,-1],[1,1,1,1]]):
 		matrix = []
 		for x in range(2):
@@ -10,7 +9,7 @@ class Tessaract(object):
 				for z in range(2):
 					for w in range(2):
 						matrix.append([dimentions[x][0],dimentions[y][1],dimentions[z][2],dimentions[w][3]])
-		self.vertex = np.asarray(matrix)
+		self.vertecies = np.asarray(matrix)
 
 	@staticmethod
 	def calculate_rotation_matrix(degree):
@@ -23,7 +22,29 @@ class Tessaract(object):
 
 	def rotate(self,degree):
 		rmatrix = Tessaract.calculate_rotation_matrix(degree)
-		self.vertex = np.dot(rmatrix,self.vertex.T).T
+		self.vertecies = np.dot(rmatrix,self.vertecies.T).T
+
+	@staticmethod
+	def calculate_projection_matrix(vertex,lw):
+		w = vertex[3]
+		projection_coefficient = 1/(lw-w)
+		primitive_matrix = np.asarray([[1,0,0,0],[0,1,0,0],[0,0,1,0]])
+		return projection_coefficient*primitive_matrix
+
+
+	def project(self,lw):
+		projection = []
+		for vertex in self.vertecies:
+			projected = np.dot(Tessaract.calculate_projection_matrix(vertex,lw),vertex.T).T
+			projection.append(projected)
+		return np.asarray(projection) 
+
+if __name__ == "__main__":
+	tessaract = Tessaract([[-1,-1,-1,-1],[1,1,1,1]])
+	print(tessaract.vertecies)
+	tessaract.rotate(math.pi*2)
+	print(tessaract.vertecies)
+	print(tessaract.project(2))
 
 
 		
